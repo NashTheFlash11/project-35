aa9eff61a0df7c10ead8a675acf2737517b225d8
NashTheFlash11 <71734964+NashTheFlash11@users.noreply.github.com>
2021-03-07 11:20:31 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+//Create variables here
+
+var dog, happyDog;
+var database;
+var foodS;
+var foodStock;
+
+function preload()
+{
+	//load images here
+  dogImage = loadImage("dogImg.png");
+  happyDogImage = loadImage("dogImg1.png");
+}
+
+function setup() {
+	database = firebase.database();
+
+  createCanvas(500, 500);
+  dog = createSprite(30, 30, 250, 250);
+
+  dog.addImage(dogImage);
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDogImage);
+  }
+  
+  
+  drawSprites();
+  //add styles here
+
+  fill("white");
+  stroke(10);
+  text("NOTE: Press UP_ARROW To Feed Drago Milk!");
+  textSize(10);
+
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+  if(x <= 0){
+    x = 0;
+  }
+  else{
+    x = x - 1;
+  }
+
+  database.ref('/').update({
+    Food: x
+  })
+}
+
+
+
